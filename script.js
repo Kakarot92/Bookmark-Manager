3d285c2e59865b114580d534ecdb2d87711d2b93
Kakarot92 <47473495+Kakarot92@users.noreply.github.com>
2026-02-20 12:12:10 +0100

    Setup bookmark form and JS references

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let bookmarks = [];
 
-console.log("Script loaded");
+let bookmarkForm = document.getElementById('bookmark-form');
+let bookmarkList = document.getElementById('bookmark-list');
+let bookmarkTitle = document.getElementById('bookmark-name');
+let bookmarkUrl = document.getElementById('bookmark-url');
+let categorySelect = document.getElementById('category-select');
+
+bookmarkForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(bookmarkForm);
+    console.log(bookmarkList);
+    console.log(bookmarkTitle);
+    console.log(bookmarkUrl);
+    console.log(categorySelect);
+});
